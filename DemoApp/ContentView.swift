ca9389c518168bd281168c314edee23fc27a597d
moonl1ght <ixav1@icloud.com>
2021-12-16 17:18:42 +0300

    Add DemoApp

---
+++ DemoApp/ContentView.swift
@@ -0,0 +1,28 @@
+//
+// Created by Alexander Lakhonin on 16.12.2021.
+// Copyright © 2021 Alexander Lakhonin. All right reserved.
+//
+
+import SwiftUI
+import AppMetrics
+import AppMetricsKit
+
+struct ContentView: View {
+  private let logger = AppLogger(subsystem: "DemoApp", category: "MainView")
+
+  var body: some View {
+    AppMetricsView()
+      .padding()
+      .onAppear(perform: onAppear)
+  }
+
+  private func onAppear() {
+    logger.debug("on appear")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,15 @@
+//
+// Created by Alexander Lakhonin on 16.12.2021.
+// Copyright © 2021 Alexander Lakhonin. All right reserved.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
